044e54fb6f4f3b262beed54988a9b3306e9c1ecf
Harshvardhan Rajchauhan <1206harsh@gmail.com>
2026-09-11 06:55:29 +0000

    ref changes to int_suppliers

--- models/intermediate/int_supplier.sql
+++ models/intermediate/int_supplier.sql
@@ -11,7 +11,7 @@
 with supplier as (
     select s.s_suppkey,n.sname , s.* exclude (s_suppkey,s_name) 
     from {{ref('stg_suppliers')}} s
-    join analytics.dbt_hrajchauhan.suppliers_names n 
+    join {{ref('suppliers_names')}} n 
     on s.s_suppkey=n.skey
     {% if is_incremental() %}
     where updated_time>(select max(updated_time) from {{ this }})
